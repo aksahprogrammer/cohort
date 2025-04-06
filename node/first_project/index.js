c9394aa25a4f7f00bd86bb3f1d872fc3163bef48
Ashok Kumar <aksahprogrammer@gmail.com>
2025-04-06 17:07:06 +0530

    Install cors package and add configuration in index.js page

--- node/first_project/index.js
+++ node/first_project/index.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT || 4000;
+
+app.use(cors({
+    origin: 'http://localhost:'+port,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
 app.get('/', (req, res) => {
     res.send('Cohort');

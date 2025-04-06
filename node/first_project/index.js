b6c615bd4c081e22ae751484f485b2982e5a32d4
Ashok Kumar <aksahprogrammer@gmail.com>
2025-04-06 16:49:22 +0530

    Add basic code of express and change type and script  in package.json file

---
+++ node/first_project/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+const port = 4000;
+
+app.get('/', (req, res) => {
+    res.send('Cohort');
+});
+
+app.listen(port, () => {
+    console.log('Server is running on port no. ' + port);
+});

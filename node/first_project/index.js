d6eaa697a32f3f9e973a2308282fd2b808663fef
Ashok Kumar <aksahprogrammer@gmail.com>
2025-04-06 16:58:15 +0530

    Install dotenv package and add env.example file adn configure dotenv file

--- node/first_project/index.js
+++ node/first_project/index.js
@@ -1,12 +1,19 @@
 import express from 'express';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
     res.send('Cohort');
 });
+
+app.get('/ashok', (req, res) => {
+    res.send('Hello Ashok');
+})
 
 app.listen(port, () => {
     console.log('Server is running on port no. ' + port);
